333555044d61776ef118243be69f99b9509e85dc
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-08-12 17:35:29 +0300

    Delete Menu

--- comango/src/components/Menu/MenuItems.ts
+++
@@ -1,6 +0,0 @@
-export const menuItems = [
-    { item: 'About', link: '#about' },
-    { item: 'Stack', link: '#stack' },
-    { item: 'Portfolio', link: '#portfolio' },
-    { item: 'Contact', link: '#contact' },
-];
