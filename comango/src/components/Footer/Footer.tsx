fe7d463354edc4ea872cdf8aade069d34c5feafe
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-08-12 17:34:41 +0300

    Temp Menu translation

--- comango/src/components/Footer/Footer.tsx
+++ comango/src/components/Footer/Footer.tsx
@@ -1,12 +1,38 @@
+import { useContext } from 'react';
+
+import { LanguageContext } from '../../context/languageContext';
 import styles from './Footer.module.css';
-import { menuItems } from '../Menu/MenuItems';
 import logo from '../../assets/images/Logo.svg';
+import { LanguageSwitcher } from '../LanguageSwitcher/LanguageSwitcher';
 
 const Footer = () => {
+    const languageContext = useContext(LanguageContext);
+
+    if (!languageContext) {
+        return null;
+    }
+
+    const { language } = languageContext;
+
+    const menuItems =
+        language === 'en'
+            ? [
+                  { item: 'About', link: '#about' },
+                  { item: 'Stack', link: '#stack' },
+                  { item: 'Portfolio', link: '#portfolio' },
+                  { item: 'Contact', link: '#contact' },
+              ]
+            : [
+                  { item: 'Про нас', link: '#about' },
+                  { item: 'Стек', link: '#stack' },
+                  { item: 'Портфоліо', link: '#portfolio' },
+                  { item: 'Контакти', link: '#contact' },
+              ];
+
     return (
         <>
             <div className={styles.wrapper}>
-                <img className={styles.logo} src={logo} alt='logo' />
+                <img className={styles.logo} src={logo} alt="logo" />
                 {menuItems.map((menuItem, index) => (
                     <a
                         href={menuItem.link}
--- comango/src/components/Header/Header.tsx
+++ comango/src/components/Header/Header.tsx
@@ -1,9 +1,34 @@
+import { useContext } from 'react';
+
+import { LanguageContext } from '../../context/languageContext';
 import styles from './Header.module.css';
 import logo from '../../assets/images/Logo.svg';
 import { LanguageSwitcher } from '../LanguageSwitcher/LanguageSwitcher';
-import { menuItems } from '../Menu/MenuItems';
 
 const Header = () => {
+    const languageContext = useContext(LanguageContext);
+
+    if (!languageContext) {
+        return null;
+    }
+
+    const { language } = languageContext;
+
+    const menuItems =
+        language === 'en'
+            ? [
+                  { item: 'About', link: '#about' },
+                  { item: 'Stack', link: '#stack' },
+                  { item: 'Portfolio', link: '#portfolio' },
+                  { item: 'Contact', link: '#contact' },
+              ]
+            : [
+                  { item: 'Про нас', link: '#about' },
+                  { item: 'Стек', link: '#stack' },
+                  { item: 'Портфоліо', link: '#portfolio' },
+                  { item: 'Контакти', link: '#contact' },
+              ];
+
     return (
         <>
             <div className={styles.wrapper}>
