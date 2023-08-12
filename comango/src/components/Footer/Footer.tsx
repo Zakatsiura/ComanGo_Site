f202f2dc1e0caa4fe80edba83bb33ebc984e6a0a
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-08-12 17:02:57 +0300

    Add logo footer

--- comango/src/components/Footer/Footer.tsx
+++ comango/src/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import styles from './Footer.module.css';
 import { menuItems } from '../Menu/MenuItems';
+import logo from '../../assets/images/Logo.svg';
 
 const Footer = () => {
     return (
         <>
             <div className={styles.wrapper}>
+                <img className={styles.logo} src={logo} />
                 {menuItems.map((menuItem, index) => (
                     <a
                         href={menuItem.link}
