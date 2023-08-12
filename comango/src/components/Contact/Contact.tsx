fcac5981fb9112bb52d5f930572c54efc19e0ff1
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-08-12 17:21:32 +0300

    Contact translate

--- comango/src/components/Contact/Contact.tsx
+++ comango/src/components/Contact/Contact.tsx
@@ -1,12 +1,23 @@
+import { useContext } from 'react';
+
+import { LanguageContext } from '../../context/languageContext';
+import { locales } from '../../locales/locales';
 import styles from './Contact.module.css';
 
 const Contact = () => {
+    const languageContext = useContext(LanguageContext);
+
+    if (!languageContext) {
+        return null;
+    }
+
+    const { language } = languageContext;
     return (
         <>
-            <div id='contact' className={styles.container}>
+            <div id="contact" className={styles.container}>
                 <div className={styles.container__bg}>
                     <div className={styles.wrapper}>
-                        <h2 className={styles.title}>Contact Us</h2>
+                        <h2 className={styles.title}>{ locales[language].contact__title}</h2>
                     </div>
                 </div>
             </div>
